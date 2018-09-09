087b69d539e4961c3aad88d2b9c09cd20683ed76
Monica Marziano <mmarziano1@gmail.com>
2018-09-09 15:34:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(file)
     parsed_file = file.split(" - ")
     artist = Artist.find_or_create_by_name(parsed_file[1])
-    song = Song.new(parsed_file[0])
+    song = self.new(parsed_file[0])
     song.artist = artist
     artist.add_song(song)
     song
