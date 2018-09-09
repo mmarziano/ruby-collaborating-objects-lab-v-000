872305c05ae947b730cd5da9f23ccceb0385c4c6
Monica Marziano <mmarziano1@gmail.com>
2018-09-09 16:23:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,8 @@ class Song
  
   
   def artist=(name)
-    @artist = artist.name
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   
   def self.new_by_filename(file)
