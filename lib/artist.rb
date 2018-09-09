89e16692ff7ca2f4d76503440b843d53f9f7718a
Monica Marziano <mmarziano1@gmail.com>
2018-09-09 16:35:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,47 +1,46 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []  #stores all instances of Artist 
 
-
-class Artist 
-  attr_accessor :name 
-  
-  @@all = []
-  
   def initialize(name)
     @name = name
     @songs = []
-
-  end 
-  
-  def songs 
-    @songs 
-  end 
-  
-  def add_song(name)
-    self.songs << name
   end
-  
-  def self.all
+
+  def add_song(song)
+    self.songs << song    #Artist.songs reader returns all songs 
+                          #stored in @songs
+  end
+
+  def save
+    @@all << self     #saves all created instances of Artist class in 
+                      #@@all class variable
+  end
+
+  def self.all      #class method all returns all instances of Artist class 
     @@all
-  end 
-  
-  def save 
-    @@all << self
-  end 
-  
-  
-  
-  def self.find_or_create_by_name(artist)
-    @@all.detect do |c|
-      if c == artist 
-        return artist.name 
-      else 
-        self.new(artist)
-      end 
+  end
+
+  def self.find_or_create_by_name(name)   #class method uses find/create class 
+                                          #methods to detect or create instances
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
     end
   end
-    
-  def print_songs 
-    @songs.each do |song|
-      puts song.name
-    end 
-  end 
-end 
+
+  def self.find(name)     #class method detects instances from @@all class variable
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.create(name)   #class method creates & stores instances vs initializing
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def print_songs #instance method iterates through @songs of an instance of Artist
+    self.songs.each {|song| song.name}
+  end
+end
