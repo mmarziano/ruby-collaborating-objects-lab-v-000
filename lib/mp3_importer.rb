850a02ac3d68d3d26ab69d1ce1470ea67e08883c
Monica Marziano <mmarziano1@gmail.com>
2018-09-09 16:36:32 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,25 +1,22 @@
+class MP3Importer
+  attr_accessor :path
 
-
-class MP3Importer 
-  attr_accessor :files, :path 
-  
-  @@all = []
-  
   def initialize(path)
     @path = path
-    self.files
-  end 
-  
-  def files 
-    @files = Dir.entries(@path)
-    @files.delete_if {|file| file == "." || file == ".."}
-    @files
-
   end
-  
-  def import 
+
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
     self.files.each do |filename|
       Song.new_by_filename(filename)
     end
-  end 
-end 
+  end
+end
+
