f0f7efa397d8ca4dc0dfd0f3d27039f0b2c5d2a0
Monica Marziano <mmarziano1@gmail.com>
2018-09-09 14:43:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,52 +1,47 @@
-class Artist 
-  attr_accessor :name, :songs
-  
-@@all = []
-@@artists = []
 
+
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
   def initialize(name)
     @name = name
     @songs = []
-  end
-  
-  
-  def self.name
-    @name
-  end
+
+  end 
   
   def songs 
-    @songs
-  end
+    @songs 
+  end 
   
-  def add_song(song)
-    @songs << song
+  def add_song(name)
+    @songs << name
   end
   
   def self.all
     @@all
   end 
   
-  def save
-    if !@@all.include?(self)
-      @@all << self
-    end
-  end
-  
-  def self.find_by_name(name)
-    self.all.detect {|artist| artist.name == name }
+  def save 
+    @@all << self
   end 
   
   
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
-    else
-      self.new(name)
+  
+  def self.find_or_create_by_name(artist)
+    @@all.detect do |c|
+      if c == artist 
+        return artist.name 
+      else 
+        self.new(artist)
+      end 
     end
   end
-  
-  def print_songs
-    @songs.each {|song| puts song.name}
-  end
-
+    
+  def print_songs 
+    @songs.each do |song|
+      puts song.name
+    end 
+  end 
 end 
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,31 +1,25 @@
+class MP3Importer 
+  attr_accessor :files, :path 
+  
+  @@all = []
+  
+  def initialize(path)
+    @path = path
+  end 
+  
+  def files 
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+    @files
 
-
-class MP3Importer
-  attr_accessor :files
+  end
   
-  @@songs = []
-  @@mp3_files = []
-  @@artist = []
-  
-    def initialize(path)
-      @files = Dir.entries(path)
-      @files
+  def import 
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+    @files.each do |file|
+      x = file[0]
+      Song.new(x)
     end
-  
-    def path
-      path = ("./spec/fixtures/mp3s")  
-    end
-    
-    def files
-       @files.each {|entry| @@mp3_files << entry if entry.include?(".mp3")}
-       @@mp3_files
-    end  
-    
-  
-    def import 
-      @@mp3_files.each do |file|
-         Song.new_by_filename(file)
-      end
-    end  
-      
-end
+  end 
+end 
